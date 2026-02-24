5f2e4c32827d616c4568d126e494fcc2acc86384
bhartijonwal <bhartijonwal28@gmail.com>
2026-02-24 22:42:34 +0530

    Update SQL schema_and_analysis.sql

--- SQL schema_and_analysis.sql
+++ SQL schema_and_analysis.sql
@@ -1,12 +1,12 @@
 -- DDL (create tables)
 create table products (productid int primary key,
-                       productname varchar (200),
+                       productname varchar (200) not null,
 					   category varchar(50),
-					   price decimal (10,2));
+					   price decimal (10,2) not null);
 
 create table customers (customerid int primary key,
-                        firstname varchar (50),
-						lastname varchar (50),
+                        firstname varchar (50) not null,
+						lastname varchar (50) not null,
 						email varchar (100),
 						phone varchar (15));
 
@@ -21,9 +21,9 @@ create table sales (saleid int primary key,
                    productid int, 
 			       customerid int,
 				   employeeid int,
-				   saledate date,
-				   quantity int,
-				   total_amount decimal(10,2),
+				   saledate date not null,
+				   quantity int not null,
+				   total_amount decimal(10,2) not null,
 				   foreign key (productid) references products(productid),
 				   foreign key (customerid) references customers(customerid),
 				   foreign key (employeeid) references employees(employeeid));
@@ -40,7 +40,7 @@ values (1,'Laptop','Electronic',1200.00),
 	   (7,'Vase','Decor',2999.00),
        (8,'RoomSpray','Fragnance',645.00);
 
-select * from products
+select * from products;
 
 insert into customers(customerid, firstname, lastname, email, phone)
 values(1,'Chris','Mathew','chris1998@gmail.com',5551234),
@@ -48,28 +48,28 @@ values(1,'Chris','Mathew','chris1998@gmail.com',5551234),
 (3,'Yuji','Itadori','yuji@gmail.com',5559871),
 (4,'Sasuke','Uzumaki','sasukeuz@gmail.com',5555672),
 (5,'Kanroji','usai','kanroji@gmail.com',5552421),
-(6,'Nezuko','Hashira','nezuko@gmail.com',42145321)
+(6,'Nezuko','Hashira','nezuko@gmail.com',42145321);
 
 select * from customers;
 
 insert into employees (employeeid, firstname, lastname, designation, email, phone)
 values(1,'Alice','Johnsone','Sales Rep','alice.johnson@gmail.com',5559876),
       (2,'Bob','Williams','Manager','bob.will@gmail.com',5558965),
-	  (3,'Macy','Spencer','Accountant','macy.spen@gmail.com',55584765)
-      (4,'Dob','Manney','Executive','Dob.Mann@gmail.com',5552455),
+	  (3,'Macy','Spencer','Accountant','macy.spen@gmail.com',55584765),
+      (4,'Dob','Manney','Executive','Dob.Mann@gmail.com',5552455)
 
 Alter table employees
-Add column Grade varchar(2)
+Add column Grade varchar(2);
 
-update employees set grade = 'A1' where designation = 'Boss'
+update employees set grade = 'A1' where designation = 'Boss';
 
-update employees set grade = 'B2' where designation = 'Manager'
+update employees set grade = 'B2' where designation = 'Manager';
 
-update employees set grade = 'C1' where designation = 'Executive'
+update employees set grade = 'C1' where designation = 'Executive';
 
-update employees set grade = 'B1' where designation = 'Sales Rep'
+update employees set grade = 'B1' where designation = 'Sales Rep';
 
-select * from employees
+select * from employees;
 
 insert into sales(saleid,productid,customerid,employeeid,saledate,quantity, total_amount)
 values (1,1,1,1,'2024-01-01',2,2400.00),
@@ -77,9 +77,9 @@ values (1,1,1,1,'2024-01-01',2,2400.00),
 	   (3,3,1,2,'2024-03-01',1,45000.00),
 	   (5,8,6,3,'2024-04-01',9,3542.00),
        (6,7,5,4,'2024-07-01',7,5534.00),
-       (7,6,5,2,'2024-05-01',10,5425.00)
+       (7,6,5,2,'2024-05-01',10,5425.00);
 
-select * from sales
+select * from sales;
 
 alter table customers
 alter column phone type varchar(9);
@@ -91,13 +91,13 @@ select productname, sum(quantity) as total_sold
 from sales
 join products on sales.productid = products.productid
 group by productname
-order by 2 desc
+order by 2 desc;
 
 --2. Top sales per product category
 select category, sum(total_amount) as total_sales
 from sales
 join products on sales.productid = products.productid
-group by category
+group by category;
 
 --3. High to low price item based on category
 select productname,category,price from products order by price desc;
